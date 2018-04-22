4ed811fb6568213057bcc2baa30a6a813bc2f382
traviswhendrix <traviswhendrix@gmail.com>
2018-04-22 15:05:13 -0700

    added master AudioEngine Prefab

---
+++ Assets/Scripts/Audio/AudioEngine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEngine : MonoBehaviour {
+
+	static AudioEngine instance = null;
+
+	
+	void Awake ()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            print ("Duplicate AudioEngine Destroyed!");
+        }
+        else 
+        {
+            instance = this;
+        }
+
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+    
+}
