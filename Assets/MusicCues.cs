468d7de27ba8fbec420191cac8f60c08cb94b7af
radaeze <33758858+radaeze@users.noreply.github.com>
2018-04-28 16:45:12 -0700

    music cues script

---
+++ Assets/MusicCues.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cradle;
+
+public class MusicCues : MonoBehaviour {
+	private SoundEngine soundEngine;
+    private MusicEngine musicEngine;
+
+	// Use this for initialization
+	void Start () {
+
+		soundEngine = Object.FindObjectOfType<SoundEngine>();
+        musicEngine = Object.FindObjectOfType<MusicEngine>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void Garage_Enter(){
+
+		musicEngine.ChangeMusicWithName("KickUp");
+	}
+	void Garage_Update()
+	{
+	// Runs every frame like a normal Update method,
+	// but only when the current passage is Garage
+	}
+
+	void Garage_Exit(){	
+		
+	}
+
+	[StoryCue("Hack_Logic", "Enter")]
+	void enterHack(){
+		// ...
+	}
+
+	[StoryCue("Hack_Logic", "Exit")]
+	void exitHack(){
+		// ...
+	}
+
+}
