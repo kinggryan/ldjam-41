0909a85fc648567693693b105b648bd3515c0fe3
radaeze <33758858+radaeze@users.noreply.github.com>
2018-05-01 15:52:19 -0700

    splits two word commands

--- Assets/DynamicCommands.cs
+++ Assets/DynamicCommands.cs
@@ -16,14 +16,29 @@ public class DynamicCommands : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 	void OnGUI() {
 		int x = 32;
 		GUI.Label (new Rect (10,10,150,20), "CMD:/", style);
 		foreach(var link in commandText){
-			x += 32;
-			GUI.Label (new Rect (10,x,150,20), link.Text, style);
+			
+			var text = link.Text;
+			//Longer commands like "GET COAT" do not enter TETRIS space
+			if (text.Contains(" ")) { 
+				x += 40;
+				string[] words = text.Split(null);
+				GUI.Label (new Rect (10,x,150,20), words[0], style);
+				GUI.Label (new Rect (10,x += 32,150,20), words[1], style);
+				
+			}
+
+			else{
+				x += 40;
+				GUI.Label (new Rect (10,x,150,20), link.Text, style);
+				
+			}
+			
 			
 		}	
 	}
