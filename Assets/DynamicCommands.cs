98f3fa0a117f50142e9c3640a69aea01bffcb41c
radaeze <33758858+radaeze@users.noreply.github.com>
2018-05-14 23:33:19 -0700

    changed commands text

--- Assets/DynamicCommands.cs
+++ Assets/DynamicCommands.cs
@@ -2,76 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cradle;
+using UnityEngine.UI;
 
-public class DynamicCommands : MonoBehaviour {
-	public IEnumerable<Cradle.StoryLink> commandText;
-	public TextrisTwinePlayer twinePlayer;
-	public Cradle.StoryVar items;
-	public GUIStyle style = new GUIStyle();
-	
-	// Use this for initialization
-	void Start () {
-		twinePlayer = Object.FindObjectOfType<TextrisTwinePlayer>();
-		commandText = twinePlayer.Story.GetCurrentLinks();
-		items = twinePlayer.Story.Vars["inv"];				
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		items = twinePlayer.Story.Vars["inv"];	
-	}
-	void OnGUI() {
-		int x = 10;
-		GUI.Label (new Rect (10,x,150,20), "CMD:/", style);
-		foreach(var link in commandText){
-			
-			var text = link.Text;
-			//Longer commands like "GET COAT" do not enter TETRIS space
-			if (text.Contains(" ")) { 
-				x += 40;
-				string[] words = text.Split(null);
-				GUI.Label (new Rect (10,x,150,20), words[0], style);
-				GUI.Label (new Rect (10,x += 32,150,20), words[1], style);
-				
-			}
+//Prints Current Available Commands as Text
+public class DynamicCommands : MonoBehaviour
+{
+    public IEnumerable<Cradle.StoryLink> links;
+    public TextrisTwinePlayer twinePlayer;
+    public Cradle.StoryVar items;
+    public Text commandText;
 
-			else{
-				x += 40;
-				GUI.Label (new Rect (10,x,150,20), link.Text, style);
-				
-			}
-			
-			
-		}	
-			x += 40;
-			GUI.Label (new Rect (10,x,150,20), "INV:/", style);
-		
+    // Use this for initialization
+    void Start()
+    {
+        twinePlayer = Object.FindObjectOfType<TextrisTwinePlayer>();
+        links = twinePlayer.Story.GetCurrentLinks();
+        items = twinePlayer.Story.Vars["inv"];
+    }
 
-			//I would like to just iterate over items but it won't let me	
-			if(items["NOTE"] == true){
-				x += 40;
-				GUI.Label (new Rect (10,x,150,20), "NOTE" , style);
-				
-			}
-			if(items["COAT"] == true){
-				x += 40;
-				GUI.Label (new Rect (10,x,150,20), "COAT" , style);
-				
-			}	
-			if(items["GUN"] == true){
-				x += 40;
-				GUI.Label (new Rect (10,x,150,20), "GUN" , style);
-				
-			}	
-			if(items["FOB"] == true){
-				x += 40;
-				GUI.Label (new Rect (10,x,150,20), "FOB" , style);
-				
-			}		
-			if(items["CURE"] == true){
-				x += 40;
-				GUI.Label (new Rect (10,x,150,20), "CURE" , style);
-				
-			}	
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        commandText.text = "CMD:/";
+        PrintCommands();
+        commandText.text += "\n INV:/";
+        PrintInv();
+
+    }
+
+    void PrintCommands()
+    {
+        links = twinePlayer.Story.GetCurrentLinks();
+        foreach (var link in links)
+        {
+            var text = link.Text;
+
+            //Longer commands like "GET COAT" do not enter TETRIS space
+            if (text.Contains(" "))
+            {
+                string[] words = text.Split(null);
+                commandText.text += "\n" + words[0];
+                commandText.text += "\n" + words[1];
+            }
+            else
+            {
+                commandText.text += "\n" + text;
+            }
+        }
+    }
+
+    void PrintInv()
+    {
+        items = twinePlayer.Story.Vars["inv"];
+
+        if (items["NOTE"] == true)
+        {
+            commandText.text += "\n NOTE";
+        }
+        if (items["COAT"] == true)
+        {
+            commandText.text += "\n COAT";
+        }
+        if (items["GUN"] == true)
+        {
+            commandText.text += "\n GUN";
+        }
+        if (items["FOB"] == true)
+        {
+            commandText.text += "\n FOB";
+        }
+        if (items["CURE"] == true)
+        {
+            commandText.text += "\n CURE";
+        }
+
+    }
 }
