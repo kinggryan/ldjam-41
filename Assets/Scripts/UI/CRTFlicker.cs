83b1010b1f0bf72b48430f1796b42fee2dbfc171
traviswhendrix <traviswhendrix@gmail.com>
2018-06-05 17:17:15 -0700

    mergey2

---
+++ Assets/Scripts/UI/CRTFlicker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CRTFlicker : MonoBehaviour {
+
+	public UnityEngine.UI.Image image;
+	public AnimationCurve flickerCurve;
+	public float curveMinSpeed;
+	public float curveMaxSpeed;
+	public float minLineBrightness = 0.3f;
+	public float maxLineBrightness = 0.33f;
+
+	private float curveVal = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		curveVal += Random.Range(curveMinSpeed,curveMaxSpeed)*Time.deltaTime;
+		var newAlpha = minLineBrightness + (maxLineBrightness - minLineBrightness)*flickerCurve.Evaluate(curveVal);
+		var newColor = image.color;
+		newColor.a = newAlpha;
+		image.color = newColor;
+	}
+}
